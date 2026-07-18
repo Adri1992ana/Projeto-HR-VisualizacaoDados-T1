9e36c1d96de3fbcc3a92f51dfc5618d6df3de3b4
Adri1992ana <silvadias.adriana@outlook.com>
2026-07-18 00:45:13 -0300

    Query1 + CSV

--- query_01.sql
+++ query_01.sql
@@ -1,2 +1,9 @@
+
+-- Query 1: Salário por Departamento e Cargo
 -- Conexões: EMPLOYEES.DEPARTMENT_ID = DEPARTMENTS.DEPARTMENT_ID e EMPLOYEES.JOB_ID = JOBS.JOB_ID
--- Filtro: WHERE SALARY IS NOT NULL
+
+SELECT salary, department_name, job_title
+FROM hr.employees
+LEFT JOIN hr.departments ON hr.employees.department_id = hr.departments.department_id
+LEFT JOIN hr.jobs ON hr.employees.job_id = hr.jobs.job_id
+WHERE salary IS NOT NULL
